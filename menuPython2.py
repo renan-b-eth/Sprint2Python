1827007c8704d2a68199208342d9d6d221f152d4
lariikawaguti <4314751@alunouninter.com>
2023-12-04 16:02:04 -0300

    commit lari

--- menuPython2.py
+++ menuPython2.py
@@ -33,25 +33,6 @@ def acessarSiteProdutos(url):
     return webbrowser.open(url)
  
  
-def sfuncionalidade():
- 
-    root2 = Tk()
-    root2.geometry("800x500")
-    root2.title("2 - Funcionalidade")
- 
-    messagebox.showinfo("Texto 2 - Funcionaldiade"," As pessoas surdas ou com deficiência auditiva que se comunicam através da Língua de Sinais, são conhecidas como “sinalizantes”. Muitas delas possuem grande dificuldade com as línguas escritas e, por isso, a comunicação em Libras proporciona liberdade e autonomia no seu dia a dia. Com acessibilidade em Libras elas conseguem receber na sua língua materna todas as informações que estão sendo transmitidas em Português. Isso é fundamental para garantir sua inclusão na sociedade, nas escolas e universidades, no mercado de trabalho, na sua lista de clientes, etc. Com base nessa informação, iremos implantar um sistema de hand talk, onde a pessoa irá selecionar a opção desejada pelo assistente virtual, e logo será direcionada.")
- 
- 
-def tfuncionalidade():
-    root2 = Tk()
-    root2.geometry("500x500")
-    root2.title("3 - Funcionalidade - Futuramente")
- 
-def qfuncionalidade():
-    root2 = Tk()
-    root2.geometry("500x500")
-    root2.title("4 - Funcionalidade - Futuramente")
- 
 def quemSomos():
     messagebox.showinfo("Quem Somos?", "Larissa Kawaguti Feliciano - 553356\nLucas Alcântara Carvalho - 95111\nRenan Bezerra dos Santos - 553228")
  
